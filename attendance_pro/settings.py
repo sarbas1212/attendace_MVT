7b7bb602b6f11fe0a7849e955ee6a9ff25080839
sarbas1212 <sarbaskallinga@gmail.com>
2026-02-19 16:48:45 +0530

    removed superuser sett

--- attendance_pro/settings.py
+++ attendance_pro/settings.py
@@ -110,13 +110,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-    if username and password and not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
